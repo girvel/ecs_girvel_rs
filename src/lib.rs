11b61f7a301b70190afe4e0f5cb34a4878a3387d
girvel <widauka@ya.ru>
2023-09-11 23:03:28 +0300

    entity!

--- src/lib.rs
+++ src/lib.rs
@@ -13,58 +13,24 @@ trait Component<T> {
     fn get_component(&self) -> &T;
 }
 
-// struct Example {
-//     name: Name,
-//     position: Position,
-// }
-//
-// impl Component<Name> for Example {
-//     fn get_component(&self) -> &Name {
-//         &self.name
-//     }
-// }
-//
-// impl Component<Position> for Example {
-//     fn get_component(&self) -> &Position {
-//         &self.position
-//     }
-// }
-
-// impl Entity {
-//     fn get<T>(&self) -> &T where Entity: Component<T> {
-//         Component::<T>::get_component(self)
-//     }
-// }
-
 trait Entity {
     fn get<T>(&self) -> &T where Self: Component<T>;
 }
 
-// impl Entity for Example {
-//     fn get<T>(&self) -> &T where Self: Component<T> {
-//         Component::<T>::get_component(self)
-//     }
-// }
+// macro_rules!
 
 macro_rules! entity {
-    ($name:ident: $component1:ident, $component2:ident) => {
+    ($name:ident: $($component:ident),*) => {
         #[allow(non_snake_case)]
         struct $name {
-            $component1: $component1,
-            $component2: $component2,
+            $( $component: $component ),*
         }
 
-        impl Component<$component1> for $name {
-            fn get_component(&self) -> &Name {
-                &self.$component1
+        $(impl Component<$component> for $name {
+            fn get_component(&self) -> &$component {
+                &self.$component
             }
-        }
-
-        impl Component<$component2> for $name {
-            fn get_component(&self) -> &Position {
-                &self.$component2
-            }
-        }
+        })*
 
         impl Entity for $name {
             fn get<T>(&self) -> &T where Self: Component<T> {
