9b1c7a9a73bd72e190d3c2ef0f3f79adb00d1c6d
girvel <widauka@ya.ru>
2023-09-11 22:47:29 +0300

    Entity trait

--- src/lib.rs
+++ src/lib.rs
@@ -13,28 +13,46 @@ trait Component<T> {
     fn get_component(&self) -> &T;
 }
 
-struct Entity {
+struct Example {
     name: Name,
     position: Position,
 }
 
-impl Component<Name> for Entity {
+impl Component<Name> for Example {
     fn get_component(&self) -> &Name {
         &self.name
     }
 }
 
-impl Component<Position> for Entity {
+impl Component<Position> for Example {
     fn get_component(&self) -> &Position {
         &self.position
     }
 }
 
-impl Entity {
-    fn get<T>(&self) -> &T where Entity: Component<T> {
+// impl Entity {
+//     fn get<T>(&self) -> &T where Entity: Component<T> {
+//         Component::<T>::get_component(self)
+//     }
+// }
+
+trait Entity {
+    fn get<T>(&self) -> &T where Self: Component<T>;
+}
+
+impl Entity for Example {
+    fn get<T>(&self) -> &T where Self: Component<T> {
         Component::<T>::get_component(self)
     }
 }
+
+// macro_rules! entity {
+//     ($name:expr, $component1:expr, $component2:expr) => {
+//
+//     }
+// }
+//
+// entity!(Example, Name, Position);
 
 #[cfg(test)]
 mod tests {
@@ -42,7 +60,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let e = Entity {
+        let e = Example {
             name: Name {name: "John Doe".to_string()},
             position: Position {x: 1, y: -1}
         };
