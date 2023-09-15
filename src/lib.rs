66dc98483533d0d49e585b665ec310742ebd9728
girvel <widauka@ya.ru>
2023-09-15 15:21:38 +0300

    Something stable

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,29 @@
-use std::rc::Rc;
+// #[macro_export] macro_rules! entity {
+//     ($name:ident: $($component:ident),*) => {
+//         #[allow(non_snake_case)]
+//         struct $name {
+//             $( $component: std::rc::Rc<$component> ),*
+//         }
+//
+//         $(impl ecs_girvel::Component<$component> for $name {
+//             fn get_component(&self) -> std::rc::Rc<$component> {
+//                 self.$component.clone()
+//             }
+//         })*
+//     };
+// }
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, ItemStruct};
+use quote::{quote, ToTokens};
 
-pub trait Component<T> {
-    fn get_component(&self) -> Rc<T>;
-}
+#[proc_macro_attribute]
+pub fn entity(_: TokenStream, input: TokenStream) -> TokenStream {
+    let item_struct = parse_macro_input!(input as ItemStruct);
 
-pub trait Entity {
-    fn get<T>(&self) -> Rc<T> where Self: Component<T>;
-}
+    let name = &item_struct.ident;
+    let fields = &item_struct.fields;
 
-impl<E: ?Sized> Entity for E {
-    fn get<T>(&self) -> Rc<T> where Self: Component<T> {Component::<T>::get_component(self)}
-}
-
-#[macro_export] macro_rules! entity {
-    ($name:ident: $($component:ident),*) => {
-        #[allow(non_snake_case)]
-        struct $name {
-            $( $component: std::rc::Rc<$component> ),*
-        }
-
-        $(impl ecs_girvel::Component<$component> for $name {
-            fn get_component(&self) -> std::rc::Rc<$component> {
-                self.$component.clone()
-            }
-        })*
-    };
+    return quote! {
+        #item_struct
+    }.into();
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,5 +1,6 @@
 use std::rc::Rc;
-use ecs_girvel::{Component, entity, Entity};
+use ecs_girvel::entity;
+use ecs_girvel_common::{Component, Entity};
 
 #[derive(PartialEq, Eq, Debug)]
 struct Name {
@@ -12,54 +13,56 @@ struct Position {
     y: i8,
 }
 
-entity!(NameSystemSubject: Name);
+// entity!(NameSystemSubject: Name);
+#[entity]
+struct NameSystemSubject(Name);
 
-impl NameSystemSubject {
-    fn from<T: Component<Name> + ?Sized>(entity: &T) -> Self {
-        Self {
-            Name: entity.get::<Name>(),
-        }
-    }
-}
-
-struct NameSystem {
-    subjects: Vec<NameSystemSubject>,
-}
-
-impl Default for NameSystem {
-    fn default() -> Self {
-        Self {
-            subjects: vec![],
-        }
-    }
-}
-
-impl NameSystem {
-    fn raise(subject: &NameSystemSubject) {
-        println!("{}", subject.get::<Name>().name)
-    }
-
-    fn update(&self) {
-        for subject in &self.subjects {
-            Self::raise(subject)
-        }
-    }
-}
-
-entity!(Example: Name, Position);
+// impl NameSystemSubject {
+//     fn from<T: Component<Name> + ?Sized>(entity: &T) -> Self {
+//         Self {
+//             Name: entity.get::<Name>(),
+//         }
+//     }
+// }
+//
+// struct NameSystem {
+//     subjects: Vec<NameSystemSubject>,
+// }
+//
+// impl Default for NameSystem {
+//     fn default() -> Self {
+//         Self {
+//             subjects: vec![],
+//         }
+//     }
+// }
+//
+// impl NameSystem {
+//     fn raise(subject: &NameSystemSubject) {
+//         println!("{}", subject.get::<Name>().name)
+//     }
+//
+//     fn update(&self) {
+//         for subject in &self.subjects {
+//             Self::raise(subject)
+//         }
+//     }
+// }
+//
+// entity!(Example: Name, Position);
 
 
 #[test]
 fn it_works() {
-    let e = Example {
-        Name: Rc::new(Name { name: "John Doe".to_string() }),
-        Position: Rc::new(Position { x: 1, y: -1 }),
-    };
-
-    assert_eq!(e.get::<Name>().name, "John Doe");
-
-    let mut name_system = NameSystem::default();
-    name_system.subjects.push(NameSystemSubject::from(&e));
-
-    name_system.update();
+    // let e = Example {
+    //     Name: Rc::new(Name { name: "John Doe".to_string() }),
+    //     Position: Rc::new(Position { x: 1, y: -1 }),
+    // };
+    //
+    // assert_eq!(e.get::<Name>().name, "John Doe");
+    //
+    // let mut name_system = NameSystem::default();
+    // name_system.subjects.push(NameSystemSubject::from(&e));
+    //
+    // name_system.update();
 }
